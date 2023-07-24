c96b36acca03cf1fa3d5774278792ad8b31fc073
hyojin_9 <koohyojin1205@gmail.com>
2023-07-24 11:45:50 +0900

    add swagger

--- src/main/java/com/my/salty_date/config/SecurityConfig.java
+++ src/main/java/com/my/salty_date/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/dating/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/swagger-resources/**").permitAll()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .anyRequest().permitAll()
                 .and()
